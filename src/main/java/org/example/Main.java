cc5e9ea89ab7e76b5097e787d35d284c33cb2da1
iamandaleao <amandabibiano144@gmail.com>
2025-12-27 13:59:44 -0300

    chore:initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> funcionarios = new ArrayList<>();
+
+        funcionarios.add("João");
+        funcionarios.add("Maria");
+        funcionarios.add("Vitor");
+        funcionarios.add("Ana");
+
+        System.out.println("Lista de funcionários: " + funcionarios);
+    }
+}
